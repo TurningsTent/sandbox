8a15c8e700ec92af5b7ff822bd500cfbe4b9f0c3
Nathan Offutt <noffutt@onetwosee.com>
2019-03-30 13:26:38 -0400

    save this thing

--- buttons.js
+++ buttons.js
@@ -4,11 +4,11 @@ var buttons = rpi_gpio_buttons([4,18,23], { mode: rpi_gpio_buttons.MODE_BCM });
 console.log('Waiting for button clicks');
 
 // bind to the clicked event and check for the assigned pins when clicked
-buttons.on('clicked', function (pin) {
+buttons.on('button_press', function (pin) {
   userClicked( pin );
 });
 
 
 function userClicked( pin ) {
-  console.log( 'clicked: ',pin );
+  console.log( 'pressed: ',pin );
 }
