1bef4103739fa171f593c6aabee17cbd7bb31930
Nathan Offutt <noffutt@onetwosee.com>
2019-03-30 14:28:08 -0400

    add ink example

---
+++ ink.js
@@ -0,0 +1,15 @@
+const inkyphat = require('inkyphat')();
+
+async function main() {
+
+  await inkyphat.init();
+
+  inkyphat.setPixel(1, 5, inkyphat.YELLOW);
+
+  inkyphat.drawRect(50, 100, inkyphat.BLACK);
+
+  await inkyphat.redraw();
+
+  await inkyphat.destroy();
+}
+main();
