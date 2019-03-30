d9fec160a32c6ee005ad33d70d1033485d0949b4
Nathan Offutt <noffutt@onetwosee.com>
2019-03-30 13:31:09 -0400

    adjust timings

--- buttons.js
+++ buttons.js
@@ -1,14 +1,24 @@
 var rpi_gpio_buttons = require('rpi-gpio-buttons');
-var buttons = rpi_gpio_buttons([4,18,23], { mode: rpi_gpio_buttons.MODE_BCM });
+var buttons = rpi_gpio_buttons([4], { mode: rpi_gpio_buttons.MODE_BCM });
+
+let count = 0;
+let clickCount = 0;
 
 console.log('Waiting for button clicks');
 
+buttons.setTiming({ pressed: 100 });
 // bind to the clicked event and check for the assigned pins when clicked
 buttons.on('button_press', function (pin) {
   userClicked( pin );
 });
 
 
+setInterval( () => {
+  count++;
+  console.log( count );
+}, 100 );
+
 function userClicked( pin ) {
-  console.log( 'pressed: ',pin );
+  clickCount++;
+  console.log( 'pressed: ',pin, clickCount );
 }
