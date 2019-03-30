8d4bd340d1862840f607ce673fdd8fd0addeeeff
Nathan Offutt <noffutt@onetwosee.com>
2019-03-30 13:21:02 -0400

    make things finally work

--- buttons.js
+++ buttons.js
@@ -1,30 +1,14 @@
 var rpi_gpio_buttons = require('rpi-gpio-buttons');
-var buttons = rpi_gpio_buttons([18,13], { mode: rpi_gpio_buttons.MODE_BCM });
+var buttons = rpi_gpio_buttons([4,18,23], { mode: rpi_gpio_buttons.MODE_BCM });
 
 console.log('Waiting for button clicks');
 
 // bind to the clicked event and check for the assigned pins when clicked
 buttons.on('clicked', function (pin) {
-  switch(pin) {
-    // Up button on pin 11 was clicked
-    case 18:
-    userClickedUp();
-    break
-    // Down button on pin 13 was clicked
-    case 13:
-    userClickedDown();
-    break;
-  }
+  userClicked( pin );
 });
 
 
-function userClickedUp() {
-  // do something here for up button
-  console.log('UP');
-}
-
-
-function userClickedDown() {
-  // do something here for down button
-  console.log('DOWN');
+function userClicked( pin ) {
+  console.log( 'clicked: ',pin );
 }
