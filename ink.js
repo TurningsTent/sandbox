c780bc385ce9f5a93916f1fd1ace8af684663afe
Nathan Offutt <noffutt@onetwosee.com>
2019-03-30 14:32:14 -0400

    larger example

--- ink.js
+++ ink.js
@@ -1,12 +1,26 @@
-const inkyphat = require('inkyphat')();
-
-async function main() {
-
+const inkyphat = require('inkyphat')({ logToStd: true });
+async function main () {
   await inkyphat.init();
 
-  inkyphat.setPixel(1, 5, inkyphat.YELLOW);
+  inkyphat.setBorder(inkyphat.BLACK);
 
-  inkyphat.drawRect(50, 100, inkyphat.BLACK);
+  // Default profile is quick (profiles only work with v2 screen)...
+  console.log('Modes: ', inkyphat.getModes());
+  inkyphat.setMode('pimoroni_yellow');
+
+  let color = inkyphat.BLACK;
+  for (let x = 0; x < (inkyphat.getWidth() / 4) - 2; x++) {
+    for (let y = 1; y < (inkyphat.getHeight() / 4) - 1; y++) {
+      inkyphat.drawRect(x * 4, y * 4, (x * 4) + 4, (y * 4) + 4, color);
+      if (color === inkyphat.BLACK) {
+        color = inkyphat.YELLOW;
+      } else if (color === inkyphat.YELLOW) {
+        color = inkyphat.WHITE;
+      } else {
+        color = inkyphat.BLACK;
+      }
+    }
+  }
 
   await inkyphat.redraw();
 
