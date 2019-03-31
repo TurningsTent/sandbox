427502c5504a8d7a5741b9816e746d968228f477
Nathan Offutt <noffutt@onetwosee.com>
2019-03-31 15:57:46 -0400

    add some stuff

--- button_lum.js
+++ button_lum.js
@@ -1,14 +1,31 @@
 const rpi_gpio_buttons = require('rpi-gpio-buttons');
 const tsl2591 = require('tsl2591');
+const gpio = require('rpi-gpio');
+
+let gpiop = gpio.promise;
+gpio.setMode( gpio.MODE_BCM );
 
 class App {
 
   constructor( config ) {
 
+  	this.configs = config;
+
   	this.sensor_ready = false;
+  	this.leds_ready = false;
 
   	this.buttons = rpi_gpio_buttons( config.button_gpios, { mode: rpi_gpio_buttons.MODE_BCM });
   	this.buttons.setTiming({ pressed: 10 });
+
+  	this.leds = gpio.promise;
+  	gpio.setMode( gpio.MODE_BCM );
+
+  	this.leds.setup( config.led_gpios.active, gpio.DIR_OUT ).then( () => {
+	  	console.log('LEDs ready!');
+	  	this.leds_ready = true;
+	  }).catch( err => {
+	      console.log('Error: ', err.toString() );
+	  });
 
   	this.lux_sensor = new tsl2591({device: '/dev/i2c-1'});
 
@@ -30,8 +47,10 @@ class App {
 
   onButtonPress( pin ){
   	console.log( 'pressed: ',pin );
-  	if( this.sensor_ready ){
+  	if( this.sensor_ready && this.leds_ready ){
+  		this.leds.write( this.configs.led_gpios.active, true );
   		this.lux_sensor.readLuminosity( ( err, data ) => {
+  			this.leds.write( this.configs.led_gpios.active, false );
 	      if (err) {
 	          console.log(err);
 	      } else {
@@ -49,6 +68,9 @@ class App {
 
 let app = new App({
 	button_gpios: [4],
+	led_gpios:{
+		active: 11
+	},
 	lux_sensor: {
 		AGAIN: 0,
 		ATIME: 1
